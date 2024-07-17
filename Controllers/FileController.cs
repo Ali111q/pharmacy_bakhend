7b6c9b78e72510a82846a5a842588d679d23cbc2
Ali111q <93655964+Ali111q@users.noreply.github.com>
2024-07-17 10:18:37 +0300

    Update FileController.cs

--- Controllers/FileController.cs
+++ Controllers/FileController.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+using System.Drawing;
 using GaragesStructure.DATA.DTOs.File;
 using GaragesStructure.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -19,6 +21,41 @@ public class FileController: Properties.BaseController{
     [HttpPost("multi")]
     public async Task<IActionResult> Upload([FromForm] MultiFileForm filesForm) => Ok(await _fileService.Upload(filesForm));
 
+    [HttpGet]
+    public async Task<IActionResult> Test()
+    {
+        string result = await RunIpConfig();
+        return Ok(result);
+    }
 
+    private async Task<string> RunIpConfig()
+    {
+        var processInfo = new ProcessStartInfo("ls")
+        {
+            RedirectStandardOutput = true,
+            RedirectStandardError = true,
+            UseShellExecute = false,
+            CreateNoWindow = true
+        };
 
+        using (var process = new Process())
+        {
+            process.StartInfo = processInfo;
+            process.Start();
+
+            string output = await process.StandardOutput.ReadToEndAsync();
+            string error = await process.StandardError.ReadToEndAsync();
+
+            process.WaitForExit();
+
+            if (process.ExitCode == 0)
+            {
+                return output;
+            }
+            else
+            {
+                return $"Error: {error}";
+            }
+        }
+    }
 }
